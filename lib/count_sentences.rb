1c8cd4eb687b89a6635953d7258674ea686fe741
ruthmesfun <ruth.mesfun@gmail.com>
2018-10-31 10:04:42 -0400

    Complete

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,18 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?('.') || question?|| exclamation?
   end
 
   def question?
-
+    self.end_with?('?') 
   end
 
   def exclamation?
-
+    self.end_with?('!')
   end
 
   def count_sentences
-
+    self.split(/[.!?]/).select{|element| !element.empty?}.length
   end
 end
